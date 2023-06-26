3bdd752085754acfb18c0b624509503364dc17de
Lukas Oppermann <lukasoppermann@github.com>
2023-06-26 13:41:11 +0200

    created separte figma build file which compiles list of files to fetch

---
+++ scripts/buildPlatforms/buildFigma.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import {PrimerStyleDictionary} from '~/src/PrimerStyleDictionary'
+import {themes} from '../themes.config'
+import {figma} from '~/src/platforms'
+import type {ConfigGeneratorOptions} from '~/src/types/StyleDictionaryConfigGenerator'
+
+export const buildFigma = (buildOptions: ConfigGeneratorOptions): void => {
+  /** -----------------------------------
+   * Colors, shadows & borders
+   * ----------------------------------- */
+  for (const {filename, source, include} of themes) {
+    // build functional scales
+    PrimerStyleDictionary.extend({
+      source,
+      include,
+      platforms: {
+        figma: figma(`figma/themes/${filename}.json`, buildOptions.prefix, buildOptions.buildPath),
+      },
+    }).buildAllPlatforms()
+  }
+
+  /** -----------------------------------
+   * Create list of files
+   * ----------------------------------- */
+  const dirs = fs.readdirSync(`${buildOptions.buildPath}figma`)
+  const files = dirs.flatMap(dir => {
+    const localFiles = fs.readdirSync(`${buildOptions.buildPath}figma/${dir}`)
+    return localFiles.map(file => `${buildOptions.buildPath}figma/${dir}/${file}`)
+  })
+  fs.writeFileSync(`${buildOptions.buildPath}figma/files.json`, JSON.stringify(files, null, 2))
+}
+
+buildFigma({
+  buildPath: 'tokens-next-private/',
+})
